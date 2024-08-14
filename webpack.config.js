29dd03abbb31be21acf50a47cf3e4b54e481f639
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2024-08-13 18:33:41 -0700

    Fix incorrect publicPath configuration, remove unused loader rules and their packages

--- webpack.config.js
+++ webpack.config.js
@@ -5,26 +5,10 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "/dist/",
   },
   devServer: {
     static: './dist',
   },
   mode: 'development',
   devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-    ],
-  },
 };
